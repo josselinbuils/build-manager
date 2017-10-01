4f9f93c09417e174f92d67c70abcc7cbdd5243b3
Josselin BUILS <josselin.buils@gmail.com>
2017-10-01 12:18:56 +0200

    Try another library

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const githubhook = require('githubhook');
-const hook = githubhook({port: 8090});
+const http = require('http');
+const createHandler = require('github-webhook-handler');
+const handler = createHandler({path: '/webhook'});
 
-hook.on('push:test', (ref, data) => {
-    console.log('hello', ref, data);
+http.createServer((req, res) => {
+    handler(req, res, () => {
+        res.statusCode = 404;
+        res.end('Not Found')
+    });
+}).listen(8090);
+
+handler.on('error', err => {
+    console.error('Error:', err.message);
 });
 
-hook.listen();
+handler.on('push', event => {
+    console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref);
+});

58444654c6434704be7545c926c30349c93d3798
Josselin BUILS <josselin.buils@gmail.com>
2017-10-01 13:24:50 +0200

    Indent to 2

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const githubhook = require('githubhook');
-const handler = githubhook({port: 8090, secret: getArg('secret')});
+const handler = githubhook({
+  path: '/webhook',
+  port: 8090,
+  secret: getArg('secret')
+});
 
 handler.on('push', (ref, data) => {
-    console.log('hello', ref, data);
+  console.log('hello', ref, data);
 });
 
 handler.listen();
 
 function getArg(name) {
-    return (process.argv.slice(2).find(val => val.indexOf(name + '=') === 0) || '').slice(name.length + 1);
+  return (process.argv.slice(2).find(val => val.indexOf(name + '=') === 0) || '').slice(name.length + 1);
 }

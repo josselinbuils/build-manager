fd5a7e0d3eb45c9d62c016ee247c80040e3ad192
Josselin BUILS <josselin.buils@gmail.com>
2017-10-01 13:37:39 +0200

    Add buildService method

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
+const {spawn} = require('child_process');
 const githubhook = require('githubhook');
+
+const repositories = ['pathFinding', 'portfolio', 'reverseProxy', 'teravia'];
+
 const handler = githubhook({
   path: '/webhook',
   port: 8090,
   secret: getArg('secret')
 });
 
-handler.on('push', (ref, data) => {
-  console.log('hello', ref, data);
+handler.on('push', (repos, ref) => {
+  if (repositories.indexOf(repos) !== -1 && ref === 'refs/heads/master') {
+    buildService(repos.toLowerCase());
+  }
 });
 
 handler.listen();
@@ -14,3 +20,12 @@ handler.listen();
 function getArg(name) {
   return (process.argv.slice(2).find(val => val.indexOf(name + '=') === 0) || '').slice(name.length + 1);
 }
+
+function buildService(name) {
+  const child = spawn('docker-compose', ['build', '--no-cache', name]);
+  child.stdout.pipe(process.stdout);
+  child.stderr.pipe(process.stderr);
+  child.on('close', code => console.info(`child process exited with code ${code}`));
+}
+
+

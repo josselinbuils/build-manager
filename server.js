f231e2605de2e96003897a67483af57e9ff3039b
Josselin BUILS <josselin.buils@gmail.com>
2018-05-30 20:18:46 +0200

    Handle ssh errors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ handler.on('push', (repos, ref) => {
     const service = `docker_${repos.replace(/-/g, '').toLowerCase()}_1`;
     // const command = `cd /home/ubuntu/docker && docker-compose build --no-cache ${service} && docker-compose up -d && docker system prune -f`;
     const command = `docker exec -it docker_${service}_1 bash -c "git checkout . && git pull && npm i && exit" && docker restart ${service}`;
-    ssh(command, config.ssh).pipe(process.stdout);
+    ssh(command, config.ssh, (err, stdout, stderr) => console.log(err, stdout, stderr));
   }
 });
 

6b3fe3cadacd07f3bdaad857a696eff293445c20
Josselin BUILS <josselin.buils@gmail.com>
2018-05-30 19:46:23 +0200

    Stop recreating everything

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const githubhook = require('githubhook');
 const {validate} = require('jsonschema');
 const ssh = require('ssh-exec');
 
+// noinspection JSFileReferences
 const config = validate(require('./config.json'), require('./config.schema.json'), {throwError: true}).instance;
 
 const handler = githubhook(config.hook);
@@ -9,7 +10,9 @@ const handler = githubhook(config.hook);
 handler.on('push', (repos, ref) => {
   if (config.repositories.indexOf(repos) !== -1 && ref === 'refs/heads/master') {
     console.log(`Build ${repos}...`);
-    const command = `cd /home/ubuntu/docker && docker-compose build --no-cache ${repos.replace(/-/g, '').toLowerCase()} && docker-compose up -d && docker system prune -f`;
+    const service = `docker_${repos.replace(/-/g, '').toLowerCase()}_1`;
+    // const command = `cd /home/ubuntu/docker && docker-compose build --no-cache ${service} && docker-compose up -d && docker system prune -f`;
+    const command = `docker exec -it docker_${service}_1 bash -c "git checkout . && git pull && npm i && exit" && docker restart ${service}`;
     ssh(command, config.ssh).pipe(process.stdout);
   }
 });

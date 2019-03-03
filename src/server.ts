80d7bda5860fbc21396abfe98cb042d5627b51bd
Josselin BUILS <josselin.buils@gmail.com>
2019-03-03 02:22:27 +0100

    Improves logs

--- src/server.ts
+++ src/server.ts
@@ -50,15 +50,12 @@ async function start(): Promise<void> {
     logs = [];
 
     dispatchLog(LogLevel.Info, `\
-
  _         _ _    _
 | |__ _  _(_) |__| |  _ __  __ _ _ _  __ _ __ _ ___ _ _
 | '_ \\ || | | / _\` | | '  \\/ _\` | ' \\/ _\` / _\` / -_) '_|
 |_.__/\\_,_|_|_\\__,_| |_|_|_\\__,_|_||_\\__,_\\__, \\___|_|
                                           |___/
-Builds ${repos}
-
-`);
+Builds ${repos}\n\n`);
 
     builder
       .build(repos)
